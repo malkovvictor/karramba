f662ef593a3b425d3d442bc16a4c6020f7a950bc
Victor Malkov <malkov.victor@gmail.com>
2019-06-23 05:43:39 +0300

    Initial commit

---
+++ desktop/src/ru/victormalkov/karramba/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.victormalkov.karramba.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.victormalkov.karramba.KarrambaMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KarrambaMain(), config);
+	}
+}
