0fa384241bd7776b8b8621e162a9b6f0e366da09
Victor Malkov <malkov.victor@gmail.com>
2019-07-21 01:40:07 +0300

    wall property move to superclass

--- core/src/ru/victormalkov/karramba/model/Wall.kt
+++ core/src/ru/victormalkov/karramba/model/Wall.kt
@@ -4,6 +4,8 @@ package ru.victormalkov.karramba.model
 abstract class Wall: CellEffect {
     override val stackable: Boolean
         get() = false
+    override val movable: Boolean
+        get() = false
 
     override fun onStack(): Boolean {
         return true
@@ -17,8 +19,6 @@ abstract class Wall: CellEffect {
 
 // просто неподвижная непрозрачная стена
 object StaticWall: Wall() {
-    override val movable: Boolean
-        get() = false
     override val decorator: Any
         get() = TODO("not implemented: return static wall texture") //To change initializer of created properties use File | Settings | File Templates.
 
@@ -26,8 +26,6 @@ object StaticWall: Wall() {
 
 // прозрачная стена, например край карты
 object TransparentWall: Wall() {
-    override val movable: Boolean
-        get() = false
     override val decorator: Any
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 
@@ -35,8 +33,6 @@ object TransparentWall: Wall() {
 
 // неподвижная стена, через которую могут падать камни
 object ThruWall: Wall() {
-    override val movable: Boolean
-        get() = false
     override val decorator: Any
         get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
 }
