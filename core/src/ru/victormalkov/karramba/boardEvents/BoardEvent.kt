e67483daf3a04851d21999d739a400e271ca8807
Victor Malkov <malkov.victor@gmail.com>
2019-08-05 18:12:31 +0300

    comments

--- core/src/ru/victormalkov/karramba/boardEvents/BoardEvent.kt
+++ core/src/ru/victormalkov/karramba/boardEvents/BoardEvent.kt
@@ -3,9 +3,11 @@ package ru.victormalkov.karramba.boardEvents
 import com.badlogic.gdx.scenes.scene2d.Actor
 import ru.victormalkov.karramba.MyGame
 
+/*
+Отображение события на доске
+ */
 open class BoardEvent(val game: MyGame): Actor() {
-    open val eventTime = 0.5f // sec
-
+    open val eventTime = 0.5f // seconds
     var myTime = 0.0f
 
     override fun act(delta: Float) {
--- desktop/src/ru/victormalkov/karramba/desktop/DesktopLauncher.java
+++ desktop/src/ru/victormalkov/karramba/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ru.victormalkov.karramba.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 1200;
 		new LwjglApplication(new MyGame(), config);
 	}
 }
